05928eba3286dfca955ff9d9213c6f650d26a60d
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-06 23:05:22 +0200

    Generate resource section + migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_205415) do
+ActiveRecord::Schema.define(version: 2022_04_06_210149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "training_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["training_id"], name: "index_sections_on_training_id"
+  end
+
+  create_table "trainings", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trainings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +48,6 @@ ActiveRecord::Schema.define(version: 2022_04_06_205415) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sections", "trainings"
+  add_foreign_key "trainings", "users"
 end
