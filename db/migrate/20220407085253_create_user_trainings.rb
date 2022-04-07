2f35cc9da499c3cbd4aa4fb14078a3048c19da99
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-07 15:05:14 +0200

    generate migration for user_training

--- db/migrate/20220407085253_create_user_trainings.rb
+++ db/migrate/20220407085253_create_user_trainings.rb
@@ -3,7 +3,7 @@ class CreateUserTrainings < ActiveRecord::Migration[5.2]
     create_table :user_trainings do |t|
       t.references :user, foreign_key: true
       t.references :training, foreign_key: true
-      t.integer :completion_rate
+      t.integer :completion_rate, default: 0
 
       t.timestamps
     end
