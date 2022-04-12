754eb66e45f8214b028edad4597ca921f5c90223
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-13 00:37:27 +0200

    Update training model w/ validates + destroy cascade on sections

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,7 @@
 class Training < ApplicationRecord
   belongs_to :user
-  has_many :sections
+  has_many :sections, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 500 }
 end
