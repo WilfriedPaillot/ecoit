77883ddaaa2c7c83a643668cf77145953c67be9f
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-13 16:18:26 +0200

    Update user migration

--- db/migrate/20220406205415_add_columns_to_users.rb
+++ db/migrate/20220406205415_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :nickname, :string, null: false
+    add_column :users, :nickname, :string
     add_index :users, :nickname, unique: true
     add_column :users, :role, :integer, default: 0
   end
--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2022_04_07_095829) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname", null: false
+    t.string "nickname"
     t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
