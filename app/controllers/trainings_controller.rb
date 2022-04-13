d0e59e0a33eb03cb67772850f139fa1f635723aa
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-13 16:25:28 +0200

    Update training#index&show controller w/ views

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,62 @@
 class TrainingsController < ApplicationController
+  before_action :set_training, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @trainings = Training.ordered
+  end
+
+  def show
+    @training = Training.find(params[:id])
+    @training_details = Section.where(training_id: @training.id)
+  end
+
+  def new
+    @training = Training.new
+  end
+
+  def edit
+  end
+
+  def create
+    @training = Training.new(training_params)
+
+    respond_to do |format|
+      if @training.save
+        format.html { redirect_to @training, notice: 'La formation a bien été créée' }
+        format.json { render :show, status: :created, location: @training }
+      else
+        format.html { render :new }
+        format.json { render json: @training.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @training.update(training_params)
+        format.html { redirect_to @training, notice: 'Training was successfully updated.' }
+        format.json { render :show, status: :ok, location: @training }
+      else
+        format.html { render :edit }
+        format.json { render json: @training.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @training.destroy
+    respond_to do |format|
+      format.html { redirect_to trainings_url, notice: 'Training was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_training
+      @training = Training.find(params[:id])
+    end
+
+    def training_params
+      params.require(:training).permit(:title, :description, :user_id)
+    end
 end
