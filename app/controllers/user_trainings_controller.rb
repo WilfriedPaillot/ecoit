50abb17141627b403f2bf5582da32e650cffde3f
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-14 02:08:18 +0200

    Update UserTraining#create method w/ all user_training's tree builds

--- app/controllers/user_trainings_controller.rb
+++ app/controllers/user_trainings_controller.rb
@@ -1,2 +1,22 @@
 class UserTrainingsController < ApplicationController
+
+  def create
+    if UserTraining.where(user_id: current_user.id, training_id: params[:training_id]).exists?
+      redirect_to trainings_path, flash: { warning: "Cette formation fait déjà parti de votre médiathèque" }
+    else
+      @user_training = UserTraining.new(user_id: current_user.id, training_id: params[:training_id])
+        Section.where(training_id: params[:training_id]).each do |section|
+          UtSection.create(user_training_id: @user_training.id)
+            Lesson.where(section_id: section.id).each do |lesson|
+              UtsLesson.create(ut_section_id: section.id)
+            end
+        end
+      if @user_training.save
+        redirect_to trainings_path, flash: { success: "La formation #{@user_training.training.title.upcase} a bien été ajoutée à votre médiathèque" }
+      else
+        flash[:danger] = "Oops, une erreur est survenue"
+      end
+    end
+  end
+
 end
