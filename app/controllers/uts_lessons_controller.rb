bb30bef798e53b681847acd2b80267c360e18881
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-18 01:10:24 +0200

    Update show & update methods for utsLessonsController

--- app/controllers/uts_lessons_controller.rb
+++ app/controllers/uts_lessons_controller.rb
@@ -1,2 +1,22 @@
 class UtsLessonsController < ApplicationController
+  def index
+  end
+
+  def show
+    @uts_lesson = UtsLesson.find(params[:id])
+    @lesson = Lesson.find_by_id(@uts_lesson.lesson_id)
+  end
+  
+  def update
+    lesson = UtsLesson.find(params[:id])
+    lesson.update(completed: true)
+    # redirect_to uts_lesson_path(lesson.id.to_i + 1)
+    if UtsLesson.where(id: lesson.id.to_i + 1).exists?
+      redirect_to uts_lesson_path(lesson.id.to_i + 1)
+    else
+      flash[:notice] = "Félicitations, vous avez terminé toutes les leçons !"
+      # redirect_to user_trainings_path(current_user)
+      return
+    end
+  end
 end
