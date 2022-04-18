2bf1a78e192ff35d13e976465801e2143f997bf6
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-18 03:02:58 +0200

    Add completion_rate draft on uts_lessons controller & ut section view

--- app/controllers/uts_lessons_controller.rb
+++ app/controllers/uts_lessons_controller.rb
@@ -10,6 +10,8 @@ class UtsLessonsController < ApplicationController
   def update
     lesson = UtsLesson.find(params[:id])
     lesson.update(completed: true)
+    section = UtSection.find_by(section_id: lesson.ut_section_id)
+    section.update(completion_rate: section.completion_rate.to_i + 1)
     # redirect_to uts_lesson_path(lesson.id.to_i + 1)
     if UtsLesson.where(id: lesson.id.to_i + 1).exists?
       redirect_to uts_lesson_path(lesson.id.to_i + 1)
