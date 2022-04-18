9bed93211f08c6c85e9e4fbc08dd7ac058f10e77
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-18 02:04:00 +0200

    Update UserTrainingsController methods

--- app/controllers/user_trainings_controller.rb
+++ app/controllers/user_trainings_controller.rb
@@ -1,22 +1,30 @@
 class UserTrainingsController < ApplicationController
+  def index
+    @user_trainings = UserTraining.where(user_id: current_user.id)
+  end
+
+  def show
+    @user_training = UserTraining.find(params[:id])
+    @user_training_sections = UtSection.where(user_training_id: @user_training.id)
+  end
 
   def create
     if UserTraining.where(user_id: current_user.id, training_id: params[:training_id]).exists?
       redirect_to trainings_path, flash: { warning: "Cette formation fait déjà parti de votre médiathèque" }
     else
-      @user_training = UserTraining.new(user_id: current_user.id, training_id: params[:training_id])
+      @user_training = UserTraining.create(user_id: current_user.id, training_id: params[:training_id])
         Section.where(training_id: params[:training_id]).each do |section|
-          UtSection.create(user_training_id: @user_training.id)
-            Lesson.where(section_id: section.id).each do |lesson|
-              UtsLesson.create(ut_section_id: section.id)
-            end
+          UtSection.create(user_training_id: @user_training.id, section_id: section.id)
+          Lesson.where(section_id: section.id).each do |lesson|
+            UtsLesson.create(ut_section_id: section.id, lesson_id: lesson.id)
+          end
         end
-      if @user_training.save
-        redirect_to trainings_path, flash: { success: "La formation #{@user_training.training.title.upcase} a bien été ajoutée à votre médiathèque" }
-      else
-        flash[:danger] = "Oops, une erreur est survenue"
-      end
+      redirect_to user_trainings_path, flash: { success: "La formation a bien été ajoutée à votre médiathèque" }
     end
   end
 
+  # private
+  # def user_training_params
+  #   params.require(:user_training).permit(:user_id, :training_id)
+  # end
 end
