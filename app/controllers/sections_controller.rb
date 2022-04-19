35183fc906820116d45bf4edd081180c1b1b0622
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-20 00:28:07 +0200

    Update section controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,24 @@
 class SectionsController < ApplicationController
+  before_action :set_section, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @section = Section.new
+  end
+
+  def create
+    @section = Section.new(section_params)
+    @section.training_id = @section[:training_id]
+    if @section.save
+      redirect_to new_lesson_path(section_id: @section.id)
+    end
+  end
+
+  private
+  def section_params
+    params.require(:section).permit(:title, :description, :training_id)
+  end
+
+  def set_section
+    @section = Section.find(params[:id])
+  end
 end
