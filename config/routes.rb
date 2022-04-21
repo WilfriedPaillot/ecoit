0395c37318659e476d9a491ab9a615aaf870a1e5
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-21 15:10:01 +0200

    Update routes.rb w/ dashboards routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :dashboards
   resources :uts_lessons
   resources :ut_sections
   resources :user_trainings
@@ -8,6 +9,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#homepage'
   
+  scope "/dashboard" do
+    get "admin", to: "dashboards#admin", as: "dashboard_admin"
+    get "student", to: "dashboards#student", as: "dashboard_student"
+    get "instructor", to: "dashboards#instructor", as: "dashboard_instructor"
+  end
+
   get 'devenez-instructeur', to: 'static_pages#instructor'
   get 'contactez-nous', to: 'static_pages#contact'
 end
