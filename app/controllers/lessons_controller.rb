5099e5b32751cf6fa3a29dd5c33c5f7c74116dcf
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-22 03:35:15 +0200

    Update lesson controller & view for edit methods

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :set_lesson, only: [:show, :edit, :update, :destroy]
+
   def new
     @lesson = Lesson.new
   end
@@ -11,12 +13,29 @@ class LessonsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @lesson.update(lesson_params)
+    if @lesson.save
+      redirect_to training_path(@lesson.section.training)
+    end
+  end
+
+  def destroy
+    @lesson.destroy
+    if @lesson.destroy
+      redirect_to training_path(@lesson.section.training)
+    end
+  end
+
   private
   def lesson_params
     params.require(:lesson).permit(:title, :content, :section_id)
   end
 
-  def set_section
-    @section = Section.find(params[:id])
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
   end
 end
