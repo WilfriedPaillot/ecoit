e6a13a893b3b35e20106f27f246bc43604f3a32f
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-25 19:08:04 +0200

    Update UtSection model w/ through relation + methods

--- app/models/ut_section.rb
+++ app/models/ut_section.rb
@@ -1,4 +1,24 @@
 class UtSection < ApplicationRecord
   belongs_to :user_training
+  belongs_to :section
   has_many :uts_lessons
+  has_many :lessons, through: :uts_lessons
+  
+  def completion_rate_percentage
+    completion_rate = uts_lessons.where(completed: true).count * 100 / uts_lessons.count
+    completion_rate.to_s + '%'
+  end
+
+  def is_empty?
+    uts_lessons.empty?
+  end
+
+  def get_first_lesson
+    uts_lessons.first.id
+  end
+
+  def get_lessons_count
+    uts_lessons.count
+  end
+
 end
