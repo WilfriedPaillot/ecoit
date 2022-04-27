3aba8463ac1340209b0129ba3893a910427f8d25
WilfriedPaillot <paillotwilfried@gmail.com>
2022-04-27 04:11:33 +0200

    Update and fix uts_lessons#update method + model

--- app/controllers/uts_lessons_controller.rb
+++ app/controllers/uts_lessons_controller.rb
@@ -7,21 +7,27 @@ class UtsLessonsController < ApplicationController
     @lesson = Lesson.find_by_id(@uts_lesson.lesson_id)
     @next = Lesson.find_by_id(@lesson.id.to_i + 1)
   end
-  
+
   def update
     lesson = UtsLesson.find(params[:id])
-
-    if lesson.completed != true
-      lesson.update(completed: true)
-      section = UtSection.find(UtsLesson.find_by(ut_section_id: lesson.ut_section_id).ut_section_id)
-      section.update(completion_rate: section.completion_rate.to_i + 1)
+    actual_section = lesson.ut_section_id
+    last_lesson = UtsLesson.where(ut_section_id: actual_section).last.id
+    last_section_of_training = UtSection.where(user_training_id: UtSection.find(actual_section).user_training_id).last.id
+    if actual_section != last_section_of_training
+      first_lesson_of_next_section = UtsLesson.where(ut_section_id: actual_section.to_i + 1).first.id
     end
-
-    if UtsLesson.where(id: lesson.id.to_i + 1).exists?
+    
+    if lesson.id != last_lesson
       redirect_to uts_lesson_path(lesson.id.to_i + 1)
+    elsif actual_section != last_section_of_training
+      redirect_to uts_lesson_path(first_lesson_of_next_section)
     else
       flash[:warning] = "Félicitations, vous avez terminé toutes les leçons de cette formation !"
       redirect_to dashboards_path
     end
+
+    lesson.update_completion
+
   end
+    
 end
--- app/models/uts_lesson.rb
+++ app/models/uts_lesson.rb
@@ -1,3 +1,11 @@
 class UtsLesson < ApplicationRecord
   belongs_to :ut_section
+  belongs_to :lesson
+
+  def update_completion
+    if self.completed != true
+      self.update(completed: true)
+    end
+  end
+
 end
